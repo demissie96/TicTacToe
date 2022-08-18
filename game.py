7edb777c1f8dc26ddfa4b83671ca5dce10452843
Johannes Demissie <demissie.johannes@gmail.com>
2022-08-18 11:02:47 +0200

    Add score counter

--- game.py
+++ game.py
@@ -6,12 +6,19 @@ class Game:
     
     def __init__(self, field):  
         
-        player_name = 'A'         
+        self.player_name = 'A'         
         game_is_on = True
         count = 0
+        a_score = 0
+        b_score = 0
+        
+        def show_scores():
+            print(f'Scores: A player: {a_score}')
+            print(f'        B player: {b_score}\n')
         
         def refresh_the_field():
             print(ascii_art.logo)
+            show_scores()
             print(f' {field[1]} | {field[2]} | {field[3]} ')
             print('-----------')
             print(f' {field[4]} | {field[5]} | {field[6]} ')
@@ -28,17 +35,24 @@ class Game:
             field[7] = '7'
             field[8] = '8'
             field[9] = '9'
+        
+        def change_player():
+            if self.player_name == 'A':
+                self.player_name = 'B'
+            else:
+                self.player_name = 'A'
+            
 
         while game_is_on:  
 
             refresh_the_field()
     
-            selected_field = input(f'\n{player_name} player --> Select a field: ')
+            selected_field = input(f'\n{self.player_name} player --> Select a field: ')
             if len(selected_field) == 1 and selected_field != '0':
                 if field[int(selected_field)] != 'O' and field[int(selected_field)] != 'X':
                 
                     try:
-                        if player_name == 'A':
+                        if self.player_name == 'A':
                             field[int(selected_field)] = 'O'    
                         else: 
                             field[int(selected_field)] = 'X'              
@@ -48,24 +62,29 @@ class Game:
                     else:
                         ConsoleClear()
                         refresh_the_field()
-                        check = CheckResult(field=field, player_name=player_name, counter=count)
+                        check = CheckResult(field=field, player_name=self.player_name, counter=count)
                         game_over = check.result()
                         
                         if game_over:
+                            if self.player_name == 'A':
+                                a_score += 1
+                            else:
+                                b_score += 1
+                            
+                            ConsoleClear()
+                            refresh_the_field()
                             continue_the_game = input("\nDo you want to countinue the game? Type 'yes' or 'no': ").lower()
+                            
                             if continue_the_game != 'yes':
                                 game_is_on = False
                             else:
                                 ConsoleClear()
                                 reset_field()
                                 count = 0
+                                change_player()
                         else: 
                             count += 1
-                            
-                            if player_name == 'A':
-                                player_name = 'B'
-                            else:
-                                player_name = 'A'
+                            change_player()
                             ConsoleClear()
                 else:
                     ConsoleClear()
@@ -75,4 +94,5 @@ class Game:
                 print('Use only valid digit!\n')
         
         ConsoleClear()
+        show_scores()
         print(ascii_art.bye)
